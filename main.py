98f19ae18d9dc988177c1cc328e731dbbe4a4880
tdawson97 <TannerDDawson@gmail.com>
2023-07-01 13:41:46 -0500

    added methods to display possible moves for player

--- main.py
+++ main.py
@@ -13,11 +13,27 @@ class Game:
         The entire window the game is played on
     border : Rect
         The border separating the board from the edge of the screen
+    pieces : list of lists
+        contains all tile objects, formatted the same as board in settings.py
+    active_player : tuple
+        the color code of the player that has the next move
+    inactive_player : tuple
+        the color code of the player that is waiting for their turn
+    available_positions : list
+        the available tiles to move on for the player that has the next move
+    black_pieces : list
+        all the black player's pieces on the board
+    white_pieces : list
+        all the white player's pieces on the board
 
     Methods
     ----------
     setup_game_board():
         sets up the initial game board
+    find_available_positions():
+        outlines all available positions for the active player
+    check_direction():
+        checks each direction from a specified piece
     run():
         keeps window open and updated until player exits
     """
@@ -32,38 +48,100 @@ class Game:
         pygame.draw.rect(self.screen, BROWN, self.border, 90, 100)
         pygame.display.set_caption('Othello')
 
+        self.pieces = []
+        self.active_player = WHITE
+        self.inactive_player = BLACK
+        self.available_positions = []
+
+        self.black_pieces = []
+        self.white_pieces = []
+
     def setup_game_board(self):
         """
-        Communicated with tile.py to create Tile objects and draw pieces
+        Communicates with tile.py to create Tile objects and draw pieces
         on the board.
         """
         for row_index, row in enumerate(board):
+            hold_pieces = []
             for i in range(8):
                 x = i * TILE_WIDTH + 100
                 y = row_index * TILE_HEIGHT + 100
                 if row[i] == '.':
-                    tile = Tile(GREY)
-                    tile.draw_piece()
-                    self.screen.blit(tile.surface, (x, y))
+                    tile = Tile(GREY, (i + 1, row_index + 1), (x, y))
                 elif row[i] == 'X':
-                    tile = Tile(BLACK)
-                    tile.draw_piece()
-                    self.screen.blit(tile.surface, (x, y))
+                    tile = Tile(BLACK, (i + 1, row_index + 1), (x, y))
+                    self.black_pieces.append(tile)
                 else:
-                    tile = Tile(WHITE)
-                    tile.draw_piece()
-                    self.screen.blit(tile.surface, (x, y))
+                    tile = Tile(WHITE, (i + 1, row_index + 1), (x, y))
+                    self.white_pieces.append(tile)
+                tile.draw_piece()
+                self.screen.blit(tile.surface, (x, y))
+                hold_pieces.append(tile)
+            self.pieces.append(hold_pieces)
+
+    def find_available_positions(self):
+        """
+        Outlines in red the pieces that the active player can make a valid move on.
+        Calls the check direction method for validation of move.
+        """
+        if self.active_player == BLACK:
+            pieces_list = self.black_pieces
+        else:
+            pieces_list = self.white_pieces
+
+        for piece in pieces_list:
+            for direction in possible_moves.values():
+                self.check_direction(piece, direction)
+
+        for piece in self.available_positions:
+            piece.draw_outline()
+            self.screen.blit(piece.surface, piece.pixel_pos)
+
+    def check_direction(self, piece, direction):
+        """
+        Checks each direction from a piece, updates self.available_positions, opponent_pieces_jumped
+        :param piece: the tile piece to check around
+        :param direction: the tuple representing which direction to check
+
+        """
+        opponent_pieces_jumped = []
+        list_pos = (piece.coord_pos[1] - 1 + direction[1])
+        item_pos = (piece.coord_pos[0] - 1 + direction[0])
+
+        # check if outside of game board
+        if list_pos not in range(0, 7) or item_pos not in range(0, 7):
+            return
+
+        check_position = self.pieces[list_pos][item_pos]
+
+        # continue checking in the same direction if check position is opponents
+        # could also try 'while check_position is in (opponent pieces) <--some variable ?
+        while check_position.color == self.inactive_player:
+            list_pos = (check_position.coord_pos[1] - 1 + direction[1])
+            item_pos = (check_position.coord_pos[0] - 1 + direction[0])
+            opponent_pieces_jumped.append(check_position)
+
+            if list_pos not in range(0, 7) or item_pos not in range(0, 7):
+                return
+
+            check_position = self.pieces[list_pos][item_pos]
+
+        if check_position.color == GREY and len(opponent_pieces_jumped) > 0:
+            self.available_positions.append(check_position)
+            return
 
     def run(self):
+        self.setup_game_board()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                self.find_available_positions()
             pygame.display.update()
 
 
 if __name__ == '__main__':
     game = Game()
-    game.setup_game_board()
     game.run()
+
--- tile.py
+++ tile.py
@@ -6,27 +6,39 @@ class Tile:
     """
     A class to represent a tile on the game board.
 
-    Attribtues
+    Attributes
     ----------
     color : tuple
         color for the piece on the tile
     surface: Surface
         to draw circle piece on Tile object
+    coord_pos: tuple
+        to represent the coordinate position of the tile
+    pixel_pos: tuple
+        to represent the position on the screen of the tile
 
     Methods
     ----------
     draw_piece():
         Draw the circular piece on the Tile object
+
+    draw_outline():
+        Draw an outline over a piece on the board
     """
-    def __init__(self, color):
+    def __init__(self, color, coord_pos, pixel_pos):
         """
         Constructs all the necessary attributes for a Tile object
         """
         self.color = color
         self.surface = pygame.Surface((TILE_WIDTH, TILE_HEIGHT), pygame.SRCALPHA)
+        self.coord_pos = coord_pos
+        self.pixel_pos = pixel_pos
 
     def draw_piece(self):
         """
         Draws the piece onto the Tile object
         """
         pygame.draw.circle(self.surface, self.color, center, radius)
+
+    def draw_outline(self, color=RED, width=5):
+        pygame.draw.circle(self.surface, color, center, radius, width)
